85c152bf5d364fa9cbb5570016f26a74903a2339
drgorillamd <dr_gorilla@protonmail.com>
2021-06-06 15:33:49 +0200

    sellingTax done - to test

--- contracts/iBNB.sol
+++ contracts/iBNB.sol
@@ -22,6 +22,7 @@ import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
  */
 
 contract iBNB is IERC20, Ownable {
+    using SafeMath for uint256;
 
     struct past_tx {
       uint256 last_timestamp;
@@ -39,8 +40,8 @@ contract iBNB is IERC20, Ownable {
 
     //@dev in percents : 0.125% - 0.25 - 0.5 - 0.75 - 0.1%
     //therefore value are div by 10**7
-    uint16[5] public selling_taxes_thresholds = [125, 250, 500, 750, 1000];
-
+    uint16[5] public selling_taxes_tranches = [125, 250, 500, 750, 1000];
+    uint8[4] public selling_taxes_rates = [2, 4, 6, 8];
 
 
     string private _name = "iBNB";
@@ -112,68 +113,95 @@ contract iBNB is IERC20, Ownable {
 //TODO: circuit breaker
     function _transfer(address sender, address recipient, uint256 amount) internal virtual {
         require(sender != address(0), "ERC20: transfer from the zero address");
-
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
 
 
         (uint112 _reserve0, uint112 _reserve1,) = pair.getReserves(); // returns reserve0, reserve1, timestamp last tx
         if(address(this) != pair.token0()) { // 0 := iBNB
           (_reserve0, _reserve1) = (_reserve1, _reserve0);
         }
-        //amount BNB = quote(amount token, reserve token, reserve bnb)
-        uint256 current_quote_in_BNB = router.quote(amount, _reserve0, _reserve1);
-
         uint256 sell_tax;
         if(sender != address(pair) && excluded_from_taxes[sender] == false) {
-          sell_tax = sellingTax(sender, amount, current_quote_in_BNB);
+          sell_tax = sellingTax(sender, amount, _reserve0);
         }
         else {
           sell_tax = 0;
         }
 
 
-
-        uint256 senderBalance = _balances[sender];
-        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        //@dev every extra token are collected into address(this), it's the balancer job to then split them
+        //between pool and reward
         _balances[sender] = senderBalance - amount;
-        _balances[recipient] += amount;
+        _balances[recipient] += amount - sell_tax ;
+        _balances[address(this)] += sell_tax;
 
         emit Transfer(sender, recipient, amount);
+        emit Transfer(sender, address(this), sell_tax);
     }
 
     //@dev take a selling tax if transfer from a non-excluded address or from the pair contract exceed
     //the thresholds defined in selling_taxes_thresholds on a daily basis (calendar)
-    function sellingTax(address sender, uint256 amount, uint256 current_quote_in_BNB) private returns(uint256) {
+    function sellingTax(address sender, uint256 amount, uint256 pool_balance) private returns(uint256) {
+//TODO Gas optim
+        uint16[5] memory _tax_tranches = selling_taxes_tranches; //gas optim
+        past_tx memory sender_last_tx = _last_tx[sender];
+        uint256 sell_tax = 0;
 
-        uint256 _genesis_timestamp = genesis_timestamp; //gas optim
-        uint16[5] _tax_thresh = selling_taxes_thresholds; //gas optim
-        past_tx sender_last_tx = _last_tx[sender];
-
-        //num days since genesis > num of days since last tx ? reinit cumsum : keep cumsum
-        if((block.timestamp - genesis_timestamp) / 8400 > (block.timestamp - _last_tx[sender].last_timestamp) / 8400) {
-          sender_last_tx[sender].cum_transfer = 0;
+        //num days since genesis > num of days since last tx ?
+        if((block.timestamp - genesis_timestamp) / 8400 > (block.timestamp - sender_last_tx.last_timestamp) / 8400) {
+          _last_tx[sender].cum_transfer = 0;
         }
 
-        if(_last_tx[sender].cum_transfer > _tax_thresh[4]) {revert("Selling tax: above max amount");}
-        else if
-        else if
-        ...
-        else revert
+        uint256 new_cum_sum = amount.add(_last_tx[sender].cum_transfer);
 
-        sender_last_tx.last_timestamp = block.timestamp;
-        uint256 a = _last_tx[sender].cum_transfer;
-        sender_last_tx.cum_transfer = a.sum(amount);
+        if(new_cum_sum > pool_balance.mul(_tax_tranches[4]).div(10**7)) {
+          revert("Selling tax: above max amount");
+        }
+        else if(new_cum_sum > pool_balance.mul(_tax_tranches[3]).div(10**7)) {
+          sell_tax = amount.mul(selling_taxes_rates[3]).div(100);
+        }
+        else if(new_cum_sum > pool_balance.mul(_tax_tranches[2]).div(10**7)) {
+          sell_tax = amount.mul(selling_taxes_rates[2]).div(100);
+        }
+        else if(new_cum_sum > pool_balance.mul(_tax_tranches[1]).div(10**7)) {
+          sell_tax = amount.mul(selling_taxes_rates[1]).div(100);
+        }
+        else if(new_cum_sum > pool_balance.mul(_tax_tranches[0]).div(10**7)) {
+          sell_tax = amount.mul(selling_taxes_rates[0]).div(100);
+        }
+        //else sell_tax stays at 0
 
+        _last_tx[sender].last_timestamp = block.timestamp;
+        _last_tx[sender].cum_transfer = sender_last_tx.cum_transfer.add(amount);
 
         return sell_tax;
     }
 
 
-    function setSellingTaxesThreshold(uint256[6] memory new_taxes) public onlyOwner {
-      selling_taxes_rates = new_taxes;
+    function setSellingTaxesTranches(uint16[5] memory new_tranches) public onlyOwner {
+      selling_taxes_tranches = new_tranches;
       emit TaxRatesChanged();
     }
 
+    function setSellingTaxesrates(uint8[4] memory new_amounts) public onlyOwner {
+      selling_taxes_rates = new_amounts;
+      emit TaxRatesChanged();
+    }
 
+//---------------------- TODO --------------------------------
+    //@dev whe triggered, will get a quote and provide liquidity with a 20% max slippage
+    function swapForLiquidity(uint256 amount) internal {
+
+      (uint112 _reserve0, uint112 _reserve1,) = pair.getReserves(); // returns reserve0, reserve1, timestamp last tx
+      if(address(this) != pair.token0()) { // 0 := iBNB
+        (_reserve0, _reserve1) = (_reserve1, _reserve0);
+      }
+      //amount BNB = quote(amount token, reserve token, reserve bnb)
+      uint256 current_quote_in_BNB = router.quote(amount, _reserve0, _reserve1);
+
+
+    }
 
     function _approve(address owner, address spender, uint256 amount) internal virtual {
         require(owner != address(0), "ERC20: approve from the zero address");
