8767070d268a5ac1adb3c9e382692c57e08320f9
drgorillamd <dr_gorilla@protonmail.com>
2021-06-10 11:32:46 +0200

    You're such a fk moron retard fk fk

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,14 +1,17 @@
 const Migrations = artifacts.require("Migrations");
 const iBNB = artifacts.require("iBNB");
+const BSC_mainnet_routeur = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const BSC_test_routeur = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1";
+
 
 module.exports = function(deployer, network) {
-  if (network="testnet") {
-    deployer.deploy(iBNB, "0xD99D1c33F9fC3444f8101754aBC46c52416550D1");
+  if (network=="testnet") {
+    deployer.deploy(iBNB, BSC_test_routeur);
   }
-  else if (network="bsc") {
-    deployer.deploy(iBNB, "0x10ED43C718714eb63d5aA57B78B54704E256024E");
+  else if (network=="bsc") {
+    deployer.deploy(iBNB, BSC_mainnet_routeur);
   }
-  else if (network="ganache") {
-    deployer.deploy(iBNB, "0x10ED43C718714eb63d5aA57B78B54704E256024E");
+  else if (network=="ganache") {
+    deployer.deploy(iBNB, BSC_mainnet_routeur);
   }
 };
